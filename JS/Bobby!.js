edac0234ae8caaef6d25143243e149798349d170
Jackson Holliday <jacksonHolliday@users.noreply.github.com>
2020-10-07 21:15:29 -0400

    update

--- JS/Bobby!.js
+++ JS/Bobby!.js
@@ -1,21 +1,22 @@
-﻿function fizzBuzzDirect() {
+﻿//main Function
+function fizzBuzzDirect() {
     //Step 1 Get Data
-    let fizz = Number(document.getElementById("txtFizzBuzzIn1").value);
-    let buzz = Number(document.getElementById("txtFizzBuzzIn2").value);
+    let fizz = Number(document.getElementById("txtFizzBuzzIn1").value);//Value of text box 1
+    let buzz = Number(document.getElementById("txtFizzBuzzIn2").value);//Value of text box 2
     //Step 2 Run data thru function
     let output = [];
-    for (let loop = 1; loop <= 100; loop++) {     
-        if (loop % fizz == 0 && loop % buzz == 0) {
+    for (let loop = 1; loop <= 100; loop++) {               //Start at 1; loop 100 times; add 1 each time
+        if (loop % fizz == 0 && loop % buzz == 0) {         //checking for divisible by both 3 & 5 
             output[loop - 1]="FizzBuzz";
-        } else if (loop % fizz == 0) {
+        } else if (loop % fizz == 0) {                      //checking for divisible by 3
             output[loop - 1]="Fizz";
-        } else if (loop % buzz == 0) {
+        } else if (loop % buzz == 0) {                      //checking for divisible by 5
             output[loop - 1]="Buzz";
         } else {
-            output[loop - 1] = loop;
+            output[loop - 1] = loop;                        //If neither, it stay a number
         }
         }   
-    //Loop over the output array but skip every 10
+    //Loop over the output array but skip every 10          //For loop for  arry output
     let finalOutput = "";
     for (let i = 0; i <= output.length - 1; i += 10) {        
         finalOutput += renderTemplate(i, output);
@@ -25,7 +26,7 @@
     document.getElementById("pFizzBuzzOut").innerHTML = finalOutput;
 }  
 
-function renderTemplate(num, output) {
+function renderTemplate(num, output) {              //Function for adding to a table
     let template = `<tr>
         <td>${output[num]}</td>
         <td>${output[num + 1]}</td>
